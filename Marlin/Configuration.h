28b3e0dab7411cfe64c1893c4078c95f963ecad7
Aurelio Bernal <aurelio@luphole.com>
2021-01-20 21:46:28 -0600

    update config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1001,14 +1001,18 @@
  *     O-- FRONT --+
  */
 // #define NOZZLE_TO_PROBE_OFFSET { -45, -7, 0 } // According to Keysey Fabrications
-#define NOZZLE_TO_PROBE_OFFSET { -44, -9, 0 } // According to TH3D
+// #define NOZZLE_TO_PROBE_OFFSET { -44, -9, 0 } // According to TH3D
+#define NOZZLE_TO_PROBE_OFFSET { -44, -5, 0 } // According to me
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-  #define PROBING_MARGIN 0
+// #define PROBING_MARGIN 0
+#define PROBING_MARGIN 50
+
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 6000
+// #define XY_PROBE_SPEED 6000
+#define XY_PROBE_SPEED (133*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST (4*60)
@@ -1323,8 +1327,8 @@
  */
 //#define AUTO_BED_LEVELING_3POINT
 //#define AUTO_BED_LEVELING_LINEAR
-//#define AUTO_BED_LEVELING_BILINEAR
-#define AUTO_BED_LEVELING_UBL
+#define AUTO_BED_LEVELING_BILINEAR
+// #define AUTO_BED_LEVELING_UBL
 //#define MESH_BED_LEVELING
 
 /**
@@ -1384,7 +1388,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
@@ -1394,7 +1398,7 @@
 
     // Beyond the probed grid, continue the implied tilt?
     // Default is to maintain the height of the nearest edge.
-    //#define EXTRAPOLATE_BEYOND_GRID
+    #define EXTRAPOLATE_BEYOND_GRID
 
     //
     // Experimental Subdivision of the grid by Catmull-Rom method.
